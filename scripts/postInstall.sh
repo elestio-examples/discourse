2debd54c2a8d0ab1be3e76bf47d69c208255e4d5
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-05 15:38:17 +0300

    update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,8 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 600s;
+
+docker-compose exec -T postgresql bash -c "psql -U postgres postgres <<EOF
+        \c bitnami_discourse
+        UPDATE site_settings SET \"value\"='${SMTP_FROM_EMAIL}' WHERE \"name\"='notification_email';
+EOF";
